eb0c669388e00b20250805768dafab97abd4f4dd
Rembrandt <REMBRANDT.BRIGHTON0049@STU.MTEC.EDU>
2020-09-17 20:55:36 -0600

    allfiles

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145')
+$("li:contains('even')").css('background-color', '#ccc')
+$("li:contains('odd')").css('background-color', '#aaa')
+$('input').attr('placeholder', 'Rembrandt')
